eea7109417117d1fbcb7ffdfe246b0f777e5569a
Chris Gianelloni <cgianelloni@cloudstruct.net>
2022-12-23 07:47:38 -0500

    feat: restore footer
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ import version from "src/version";
 
 const Footer = () => {
   return (
-    // <div className={"background text-lg p-5 shadow-2xl rounded-2xl w-56 h-fit"}>
-    <div className="mt-auto w-full flex flex-col items-center opacity-50 p-5 shadow-2xl rounded-2xl text-xxs sm:text-sm">
-      <div className="break-all text-center">UI version: {version}</div>
+    <div className="background fixed inset-x-0 bottom-0 mt-auto w-full flex flex-row items-center opacity-50 p-5 text-xxs sm:text-sm">
+      <div className="ml-2 mr-2 text-right flex flex-row w-fit">© 2022 CloudStruct, LLC.</div>
+      <div className="ml-2 mr-2 text-left flex flex-row w-fit">UI version: {version}</div>
     </div>
   );
 };
