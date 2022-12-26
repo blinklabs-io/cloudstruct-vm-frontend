fb1f64b9974be105713b26d733625af4614c1e5a
Chris Gianelloni <cgianelloni@cloudstruct.net>
2022-12-26 13:18:03 -0500

    feat: featured pools
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- client/src/pages/Cardano/Pools/index.tsx
+++ client/src/pages/Cardano/Pools/index.tsx
@@ -1,10 +1,13 @@
 import Pool from "src/components/Pool";
+import FeaturedPool from "src/components/FeaturedPool";
 import usePools from "src/hooks/cardano/pools/usePools";
+import useFeaturedPools from "src/hooks/cardano/pools/useFeaturedPools";
 import Loading from "src/pages/Loading";
 import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Pools = () => {
+  const { featPools, featLoading } = useFeaturedPools();
   const { pools, loading } = usePools();
 
   return loading ? (
@@ -12,6 +15,11 @@ const Pools = () => {
   ) : (
     <>
       <p className="text-3xl">Eligible Stake Pools</p>
+      <div className="flex flex-col gap-4">
+        {featPools.map((pool) => (
+          <FeaturedPool key={pool.ticker} pool={pool}></FeaturedPool>
+        ))}
+      </div>
       <div
         className={
           "background shadow-2xl rounded-2xl p-5 flex flex-row items-center gap-2"
