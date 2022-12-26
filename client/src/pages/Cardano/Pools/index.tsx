3432bf3682bb9dbf4c4f8d3f31ab91924fef77f4
Chris Gianelloni <cgianelloni@cloudstruct.net>
2022-12-26 09:31:26 -0500

    feat: add notice to pools
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- client/src/pages/Cardano/Pools/index.tsx
+++ client/src/pages/Cardano/Pools/index.tsx
@@ -1,6 +1,8 @@
 import Pool from "src/components/Pool";
 import usePools from "src/hooks/cardano/pools/usePools";
 import Loading from "src/pages/Loading";
+import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Pools = () => {
   const { pools, loading } = usePools();
@@ -9,7 +11,18 @@ const Pools = () => {
     <Loading></Loading>
   ) : (
     <>
-      <p className="text-3xl">Participating Pools</p>
+      <p className="text-3xl">Eligible Stake Pools</p>
+      <div
+        className={
+          "background shadow-2xl rounded-2xl p-5 flex flex-row items-center gap-2"
+        }
+      >
+        <div className="text-premium">
+          <FontAwesomeIcon icon={faLightbulb} />
+        </div>
+        Delegators to stake pools listed below can use this claims service
+        at no additional charge
+      </div>
       <div className="flex flex-col gap-4">
         {pools.map((pool) => (
           <Pool key={pool.ticker} pool={pool}></Pool>
